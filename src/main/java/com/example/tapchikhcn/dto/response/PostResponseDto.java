84d4b28dd6397c5cfe3fc396c6911346d3520ef2
uyen134 <128410231+uyen134@users.noreply.github.com>
2024-08-05 18:33:44 +0700

    CRUD post

--- src/main/java/com/example/tapchikhcn/dto/response/PostResponseDto.java
+++ src/main/java/com/example/tapchikhcn/dto/response/PostResponseDto.java
@@ -1,5 +1,6 @@
 package com.example.tapchikhcn.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,14 +20,17 @@ public class PostResponseDto
     private String summary;
     private String body;
     private int view = 0;
-    private UserResponseDto user;
+    private int user;
     private CategoryResponseDto category;
     private String image;
     private String status = "disable";
     private boolean selected = true;
     private boolean breakingNews = true;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private Date publishedAt;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private Date createdAt;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private Date updatedAt;
     private String authorName;
     private String file;
--- src/main/java/com/example/tapchikhcn/services/Impl/CategoryServiceImpl.java
+++ src/main/java/com/example/tapchikhcn/services/Impl/CategoryServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.tapchikhcn.services.Impl;
 
+import com.example.tapchikhcn.dto.response.CategoryResponseDto;
+import com.example.tapchikhcn.entity.CategoryEntity;
 import com.example.tapchikhcn.services.CategoryService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -7,4 +9,10 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
+    public CategoryResponseDto entityToResponseMapper(CategoryEntity entity) {
+        CategoryResponseDto dto = new CategoryResponseDto();
+        dto.setId(entity.getId());
+        dto.setName(entity.getName());  // Assume CategoryEntity has a name property
+        return dto;
+    }
 }
